4729beaf5e0a28d448a8bb36e4617ed8b0da945f
Rahamat-prog <101989050+Rahamat-prog@users.noreply.github.com>
2024-04-13 14:14:26 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+// variables 
+
+const accountID = 9847387;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
+
+// now change the value of these variables 
+
+accountID
